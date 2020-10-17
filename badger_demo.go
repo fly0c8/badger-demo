33e862daf4756a744913a33054de886a3a8f6656
Arnold Angerer <arnold.angerer@gmail.com>
2020-10-17 15:08:03 +0200

    find contracts

--- badger_demo.go
+++ badger_demo.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/gob"
 	"fmt"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 	"log"
 	badger "github.com/dgraph-io/badger/v2"
-
+	"os"
+	"strings"
 )
 
 type Contract struct {
@@ -36,17 +36,78 @@ var (
 	}
 )
 
+func createManyContracts(db *badger.DB)  {
+		if err := db.Update(func(txn *badger.Txn) error {
+			for i:=0; i<60000; i++ {
+				var buf bytes.Buffer
+				enc := gob.NewEncoder(&buf)
+				c := Contract{
+					Id:          fmt.Sprintf("Contract:%d", i),
+					Contractor:  fmt.Sprintf("Contractor:%d", i),
+					Identifiers: []string {
+						"lpn1","lpn2","lpn3",
+					},
+				}
+				enc.Encode(c)
+				if err := txn.Set([]byte(c.Id), buf.Bytes()); err != nil {
+					return err
+				}
+			}
+			return nil
+		}); err !=nil {
+			log.Fatal(err)
+		}
+}
+
 func main() {
-	db, err := badger.Open(badger.DefaultOptions("contracts.db"))
+	db, err := badger.Open(badger.DefaultOptions("./contracts"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	createContracts(db)
+	//createContracts(db, contracts)
+	//createManyContracts(db)
+
+	//if c, err := readContract(db, "Contract:6666"); err == nil {
+	//	fmt.Printf("Found contract: %+v\n", c)
+	//}
+	//
+	//if c, err := readContract(db, "Contract:47111"); err == nil {
+	//	fmt.Printf("Found contract: %+v\n", c)
+	//}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter ContractID:")
+		contractID, _ := reader.ReadString('\n')
+		contractID = "Contract:"+strings.TrimSpace(contractID)
+		fmt.Println("Searching for:"+contractID)
+		if c, err := readContract(db, contractID); err != nil {
+			fmt.Println("No contract found:"+err.Error())
+		} else {
+			fmt.Printf("Found contract: %+v\n", c)
+		}
+	}
+
+
 }
 
-
-
-func createContracts(db *badger.DB) {
+func readContract(db *badger.DB, contractId string) (*Contract, error) {
+	var contract Contract
+	if err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(contractId))
+		if err != nil {
+			return err
+		}
+		return item.Value(func(val []byte) error {
+			return gob.NewDecoder(bytes.NewReader(val)).Decode(&contract)
+		});
+	}); err != nil {
+		return nil, err
+	} else {
+		return &contract, nil
+	}
+}
+func createContracts(db *badger.DB, contracts []Contract) {
 	for _, c := range contracts {
 		if err := db.Update(func(txn *badger.Txn) error {
 			var buf bytes.Buffer
@@ -55,6 +116,7 @@ func createContracts(db *badger.DB) {
 			if err := txn.Set([]byte(c.Id), buf.Bytes()); err != nil {
 				return err
 			}
+			return nil
 		}); err !=nil {
 			log.Fatal(err)
 		}
--- go.mod
+++ go.mod
@@ -1,9 +1,7 @@
-module spark.skidata.com/apiserver
+module spark.skidata.com/badgerdemo
 
 go 1.14
 
 require (
 	github.com/dgraph-io/badger/v2 v2.2007.2
-	gorm.io/driver/sqlite v1.1.3
-	gorm.io/gorm v1.20.2
 )
